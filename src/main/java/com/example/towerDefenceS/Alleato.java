eb4590f3ffe3bc53da62364638965a87fae958c8
Bogvv <bogdan.rusu.201008@gmail.com>
2026-05-17 23:19:28 +0200

    classe alleato

---
+++ src/main/java/com/example/towerDefenceS/Alleato.java
@@ -0,0 +1,29 @@
+package com.example.towerDefenceS;
+
+public class Alleato extends Personaggio{
+    public Alleato(int tipo, double y){
+        super(100,1,2,60, "temp.png", "temp.png", true);
+        setTranslateX(50);setTranslateX(y);
+        switch (tipo){
+            case 1:
+                configura("walk_contadine.png", "slash_contadine.png", 120, 2, 2, 70);
+                break;
+            case 2:
+                configura("walk_arciere.png", "shoot_arciere.png", 70, 1.5, 2.5, 250);
+                break;
+            case 3:
+                configura("walk_knight.png", "slash_knight.png", 50, 4, 1.8, 180);
+                break;
+        }
+    }
+
+    private void configura(String w, String a, double h, double d, double v, double r){
+        this.getChildren().clear();
+        javafx.scene.image.ImageView vW = new javafx.scene.image.ImageView(new javafx.scene.image.Image("file:assets/" + w));
+        javafx.scene.image.ImageView vA = new javafx.scene.image.ImageView(new javafx.scene.image.Image("file:assets/" + a));
+        vW.setFitWidth(100); vW.setFitHeight(100);
+        vA.setFitWidth(100); vA.setFitHeight(100);
+        this.getChildren().addAll(vW, vA);
+        this.HP = h; this.danno = d; this.speed = v; this.range = r;
+    }
+}
